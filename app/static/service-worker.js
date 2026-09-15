c001815dfc2fbb52f37eecd2887591db7b7a49ae
akarshjain05 <akarshjain2006@gmail.com>
2026-09-15 15:20:51 +0530

    Bump service worker cache to v3 to invalidate cached frontend files

--- app/static/service-worker.js
+++ app/static/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "evenly-shell-v2";
+const CACHE_NAME = "evenly-shell-v3";
 const SHELL_FILES = ["/", "/style.css", "/app.js", "/manifest.json"];
 
 self.addEventListener("install", (event) => {
