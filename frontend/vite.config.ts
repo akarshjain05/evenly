3082ff528630e95dd69f7e1ea20b825be0000986
akarshjain05 <akarshjain2006@gmail.com>
2026-09-16 23:52:27 +0530

    style(frontend): use transparent SVG for browser favicon

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      includeAssets: ['favicon.svg'],
       manifest: {
         name: 'Evenly',
         short_name: 'Evenly',
